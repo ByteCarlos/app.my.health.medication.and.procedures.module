b7a8c97b48f78f21660cd118667c330614484a79
Carlos <109424074+CarlosDcomp@users.noreply.github.com>
2023-03-30 22:14:27 -0300

    WIP

--- src/modules/remedies/views/form/schedule/index.jsx
+++ src/modules/remedies/views/form/schedule/index.jsx
@@ -1,6 +1,16 @@
 import React, { useState } from 'react'
-import { Modal, View, Text, TextInput, Button, StyleSheet } from 'react-native'
+import {
+  Modal,
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+} from 'react-native'
 import { Picker } from '@react-native-picker/picker'
+import DateInput from '../DateInput'
+import TimeInput from '../TimeInput'
+import { styles } from '../../../css/form/schedule'
 
 const SchedulingModal = ({ visible, onClose }) => {
   const handleSubmit = () => {
@@ -11,62 +21,53 @@ const SchedulingModal = ({ visible, onClose }) => {
   const [periodo, setPeriodo] = useState(null)
 
   return (
-    <Modal visible={visible} animationType="fade" transparent>
-      <View style={styles.modalContainer}>
-        <View style={styles.modalContent}>
-          <Text style={styles.modalTitle}>Novo Agendamento</Text>
-          <TextInput style={styles.input} placeholder="Descrição" />
-          <TextInput style={styles.input} placeholder="Data de início" />
-          <TextInput style={styles.input} placeholder="Data de fim" />
-          <TextInput style={styles.input} placeholder="Horário" />
-          <View style={styles.input}>
-            <Picker
-              style={{ width: '100%', heigth: 90 }}
-              selectedValue={periodo}
-              onValueChange={(item, indexItem) => {
-                setPeriodo(item)
-              }}
-            >
-              <Picker.Item key={0} value={null} label="Selecione o período" />
-              <Picker.Item key={0} value="Manhã" label="Manhã" />
-              <Picker.Item key={0} value="Tarde" label="Tarde" />
-              <Picker.Item key={0} value="Noite" label="Noite" />
-            </Picker>
+    <Modal
+      visible={visible}
+      animationType="fade"
+      onRequestClose={onClose}
+      transparent
+    >
+      <TouchableWithoutFeedback onPress={onClose}>
+        <View style={styles.modalContainer}>
+          <View style={styles.modalContent}>
+            <Text style={styles.modalTitle}>Novo Agendamento</Text>
+            <TextInput style={styles.input} placeholder="Descrição" />
+            <View style={styles.input}>
+              <DateInput placeholder={'Data de início'} />
+            </View>
+            <View style={styles.input}>
+              <DateInput placeholder={'Data de fim'} />
+            </View>
+            <View style={styles.input}>
+              <TimeInput placeholder={'Horário'} />
+            </View>
+            <View style={styles.input}>
+              <Picker
+                style={{ width: '100%' }}
+                selectedValue={periodo}
+                onValueChange={(item, indexItem) => {
+                  setPeriodo(item)
+                }}
+              >
+                <Picker.Item key={0} value={null} label="Selecione o período" />
+                <Picker.Item key={0} value="Manhã" label="Manhã" />
+                <Picker.Item key={0} value="Tarde" label="Tarde" />
+                <Picker.Item key={0} value="Noite" label="Noite" />
+              </Picker>
+            </View>
+            <View style={styles.containerSchedulingButton}>
+              <TouchableOpacity
+                style={styles.schedulingButton}
+                onPress={handleSubmit}
+              >
+                <Text style={styles.schedulingButtonText}>Agendar</Text>
+              </TouchableOpacity>
+            </View>
           </View>
-          <Button title="Agendar" onPress={handleSubmit} />
         </View>
-      </View>
+      </TouchableWithoutFeedback>
     </Modal>
   )
 }
-
-const styles = StyleSheet.create({
-  modalContainer: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'rgba(0, 0, 0, 0.5)',
-  },
-  modalContent: {
-    backgroundColor: 'white',
-    width: '70%',
-    padding: 20,
-    borderRadius: 10,
-    alignItems: 'center',
-  },
-  modalTitle: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    marginBottom: 20,
-  },
-  input: {
-    borderWidth: 1,
-    borderColor: 'gray',
-    borderRadius: 5,
-    padding: 10,
-    marginBottom: 10,
-    width: '100%',
-  },
-})
 
 export default SchedulingModal
