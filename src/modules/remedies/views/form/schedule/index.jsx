00ec093f6396c7317cf088f8cd02ba06c0faa6d4
Carlos <109424074+CarlosDcomp@users.noreply.github.com>
2023-03-30 21:37:58 -0300

    picker add

--- src/modules/remedies/views/form/schedule/index.jsx
+++ src/modules/remedies/views/form/schedule/index.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { Modal, View, Text, TextInput, Button, StyleSheet } from 'react-native'
+import { Picker } from '@react-native-picker/picker'
 
 const SchedulingModal = ({ visible, onClose }) => {
   const handleSubmit = () => {
@@ -7,15 +8,31 @@ const SchedulingModal = ({ visible, onClose }) => {
     onClose()
   }
 
+  const [periodo, setPeriodo] = useState(null)
+
   return (
     <Modal visible={visible} animationType="fade" transparent>
       <View style={styles.modalContainer}>
         <View style={styles.modalContent}>
           <Text style={styles.modalTitle}>Novo Agendamento</Text>
+          <TextInput style={styles.input} placeholder="Descrição" />
           <TextInput style={styles.input} placeholder="Data de início" />
           <TextInput style={styles.input} placeholder="Data de fim" />
           <TextInput style={styles.input} placeholder="Horário" />
-          <TextInput style={styles.input} placeholder="Período" />
+          <View style={styles.input}>
+            <Picker
+              style={{ width: '100%', heigth: 90 }}
+              selectedValue={periodo}
+              onValueChange={(item, indexItem) => {
+                setPeriodo(item)
+              }}
+            >
+              <Picker.Item key={0} value={null} label="Selecione o período" />
+              <Picker.Item key={0} value="Manhã" label="Manhã" />
+              <Picker.Item key={0} value="Tarde" label="Tarde" />
+              <Picker.Item key={0} value="Noite" label="Noite" />
+            </Picker>
+          </View>
           <Button title="Agendar" onPress={handleSubmit} />
         </View>
       </View>
